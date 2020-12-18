a75fc6d0c245d793147cc377591db4beac06c5d5
Conall Heffernan <conallh1234@hotmail.com>
2020-12-18 19:25:33 +0000

    added routing for add, update, delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
 
 dotenv.config();
 
 const app = express();
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 const port = process.env.PORT;
 
