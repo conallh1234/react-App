56512c3922bc26e8ea0a5ea04a512cad854e7744
Conall Heffernan <conallh1234@hotmail.com>
2020-12-28 20:22:20 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -23,4 +23,22 @@ router.put('/:id',  (req, res, next) => {
     })
     .then(user => res.json(200, user)).catch(next);
 });
+
+router.post('/:userName/favourites', (req, res, next) => {
+  const newFavourite = req.body;
+  const query = {username: req.params.userName};
+  if (newFavourite && newFavourite.id) {
+    User.find(query).then(
+      user => {
+        (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+        User.findOneAndUpdate(query, {favourites:user.favourites}, {
+          new: true
+        }).then(user => res.status(201).send(user));
+      }
+    ).catch(next);
+  } else {
+      res.status(401).send("Unable to find user")
+  }
+});
+
 export default router;
